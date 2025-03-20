35798aa3dd4a1426fce708a6a3d4d452f2a05156
Alexandr Knyazev <a.knyazev944@gmail.com>
2025-03-20 19:20:19 +0300

    обновил build.sh 2.0

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH=$PATH:$HOME/.local/bin/env
+source $HOME/.local/bin/env
+echo $PATH
+which uv  # Должно вывести /opt/render/.local/bin/uv
+uv --version  # Должно вывести версию, например, 0.6.8
 make install
