a6f53afb4c9f71d89ba709c8e0280d5e9093e7b6
Alexandr Knyazev <a.knyazev944@gmail.com>
2025-03-31 14:34:34 +0300

    исправил ошибки деплоя

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
-. $HOME/.local/bin/env
 uv --version
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install
+psql -a -d "$DATABASE_URL" -f database.sql
