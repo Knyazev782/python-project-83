ff3d29ba6023d11180d51c107499e30dd862a37a
Alexandr Knyazev <a.knyazev944@gmail.com>
2025-04-08 14:33:21 +0300

    ошибки с подключением к БД

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:pass@postgres:5432/postgres')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:pass@localhost:5432/postgres')
 
 
 class DatabaseConnection:
